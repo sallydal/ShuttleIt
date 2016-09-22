a8300f436835cf5846ed2c317b845e01292c87b3
sallydal <sally.s.moon@gmail.com>
2016-09-22 17:44:38 -0400

    lets retry this

---
+++ ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Shuttle It
+//
+//  Created by Sally Moon on 9/21/16.
+//  Copyright © 2016 WYA. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
+        /*let initialLocation = CLLocation(latitude: 40.0370, longitude: 75.3444)
+        let regionRadius: CLLocationDistance = 1000
+        func centerMapOnLocation(location: CLLocation) {
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+        centerMapOnLocation(initialLocation)*/
+    }
+    
+    //MARK: = Location Delegate Methods
+    
+    func locationManager (manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.latitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError){
+        print ("Errors:" + error.localizedDescription)
+    }
+
+
+}
+
